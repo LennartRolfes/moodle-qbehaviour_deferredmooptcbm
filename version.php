a085a6a9183af807942c5abdc3b43ebb25c64daf
Lennart Rolfes <lennart.rolfes@stud.hs-hannover.de>
2022-08-04 00:00:42 +0200

    fix version

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_deferredmooptcbm';
 $plugin->release   = '0.1.1';
-$plugin->version   = 2021052502;
+$plugin->version   = 2022080300;
 $plugin->requires  = 2021051100;
 $plugin->dependencies = array(
     'qbehaviour_deferredmoopt' => 2021120300
